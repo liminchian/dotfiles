b2bd20db564863cebf5d95e169ea6c045dffef85
liminchian <liminchian@gmail.com>
2022-11-28 20:06:48 +0800

    加入新設置

--- firefox/user.js
+++ firefox/user.js
@@ -1,2 +1,53 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
-user_pref("svg.context-properties.content.enabled", true);
+// ** Browser Options **********************************************************
+user_pref("browser.search.widget.inNavBar",            true);
+
+// == Theme Distribution Settings ==============================================
+user_pref("userChrome.tab.connect_to_window",          true); // Original, Photon
+user_pref("userChrome.tab.color_like_toolbar",         true); // Original, Photon
+
+user_pref("userChrome.tab.lepton_like_padding",       false); // Original
+user_pref("userChrome.tab.photon_like_padding",        true); // Photon
+
+user_pref("userChrome.tab.dynamic_separator",         false); // Original, Proton
+user_pref("userChrome.tab.static_separator",           true); // Photon
+user_pref("userChrome.tab.static_separator.selected_accent", false); // Just option
+
+user_pref("userChrome.tab.newtab_button_like_tab",    false); // Original
+user_pref("userChrome.tab.newtab_button_smaller",      true); // Photon
+user_pref("userChrome.tab.newtab_button_proton",      false); // Proton
+
+user_pref("userChrome.icon.panel_full",               false); // Original, Proton
+user_pref("userChrome.icon.panel_photon",              true); // Photon
+
+// Original Only
+user_pref("userChrome.tab.box_shadow",                false);
+user_pref("userChrome.tab.bottom_rounded_corner",     false);
+
+// Photon Only
+user_pref("userChrome.tab.photon_like_contextline",    true);
+user_pref("userChrome.rounding.square_tab",            true);
+
+// == Theme Custom Settings ====================================================
+// -- User Chrome --------------------------------------------------------------
+user_pref("userChrome.decoration.panel_button_separator",   true);
+user_pref("userChrome.decoration.panel_arrow",              true);
+
+user_pref("userChrome.combined.nav_button",                 true);
+user_pref("userChrome.combined.nav_button.normal_forward",  true);
+
+user_pref("userChrome.padding.urlView_expanding",           true);
+user_pref("userChrome.padding.urlView_result",              true);
+user_pref("userChrome.padding.toolbar_button.compact",      true);
+
+user_pref("userChrome.urlView.always_show_page_actions",    true);
+user_pref("userChrome.urlView.move_icon_to_left",           true);
+
+user_pref("userChrome.rounding.square_panel",               true);
+user_pref("userChrome.rounding.square_panelitem",           true);
+
+user_pref("userChrome.panel.full_width_separator",          true);
+user_pref("userChrome.panel.full_width_padding",            true);
+
+user_pref("userChrome.tabbar.multi_row",                    true);
+user_pref("userChrome.bookmarkbar.multi_row",               true);
+
