8ab2b6fc38242aaa29e45f846bfe0b7ddc43dfee
liminchian <liminchian@gmail.com>
2022-12-02 18:52:32 +0800

    加入設置

--- image/livewallpaper.sh
+++ image/livewallpaper.sh
@@ -4,15 +4,15 @@ FILE="$HOME/.cache/mpvpaper.xyz"
 VIDEO="$HOME/.config/image/chill.mp4"
 
 run() {
-  mpvpaper -spf -o "input-ipc-server=/tmp/mpv-socket no-audio --loop-playlist --shuffle" HDMI-A-1 "$VIDEO"
+	mpvpaper -spf -o "input-ipc-server=/tmp/mpv-socket no-audio --loop-playlist --shuffle" HDMI-A-1 "$VIDEO"
 }
 
 if [[ ! $(pidof mpvpaper) ]]; then
-  if [[ ! -f "$FILE" ]]; then
-    touch "$FILE"
-  fi
-  run
+	if [[ ! -f "$FILE" ]]; then
+		touch "$FILE"
+	fi
+	run
 else
-  pkill mpvpaper
-  rm "$FILE"
+	pkill mpvpaper
+	rm "$FILE"
 fi
