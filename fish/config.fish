986e509eff5dbb116b896d3a869826dd62c1597d
liminchian <liminchian@gmail.com>
2022-12-03 10:34:36 +0800

    Last Sync: 2022-12-03 10:34:36

--- fish/config.fish
+++ fish/config.fish
@@ -2,38 +2,48 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if status is-login
+if status --is-login
   if test -z "$DISPLAY" && test "$(tty)" = "/dev/tty1"
     set -gx LANG zh_TW.UTF-8
-    set -gx PATH $PATH ~/.local/bin
+    set -gx LIBVA_DRIVER_NAME nvidia
+    set -gx XDG_SESSION_TYPE wayland
+    set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+    set -gx MOZ_ENABLE_WAYLAND 1
+    set -gx GDK_BACKEND wayland
+    set -gx GBM_BACKEND nvidia-drm
+    set -gx __GLX_VENDOR_LIVRARY_NAME nvidia
+    set -gx WLR_NO_HARDWARE_CURSORS 1
+    set -gx CLUTER_BACKEND wayland
+    set -gx QT_QPA_PLATFORM wayland
+
+    set -gx GDK_SCALE 1
+    set -gx XCURSOR_SIZE 20
+    set -gx RUST_BACKTRACE full
+    set -gx DESKTOP_SESSION Hyprland
+    set -gx JUPYTERLAB_DIR $HOME/.local/share/jupyter/lab
+    set -gx SKIM_DEFAULT_OPTIONS "--ansi --regex"
+    set -gx AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+
+    set -gx GTK_IM_MODULE fcitx
+    set -gx QT_IM_MODULE fcitx
+    set -gx SDL_IM_MODULE fcitx
+    set -gx GLFW_IM_MODULE ibus
+    set -gx OBSIDIAN $HOME/Desktop/obsidian-notes
+
     exec Hyprland
   end
 end
 
-fish_add_path -p ~/.bashrc ~/.bash_profile /etc/profile /etc/bash.bashrc
+if test -e "$AUTOJUMP_PATH"
+  source "$AUTOJUMP_PATH"
+end
 
-set -gx LIBVA_DRIVER_NAME nvidia
-set -gx XDG_SESSION_TYPE wayland
-set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
-set -gx MOZ_ENABLE_WAYLAND 1
-set -gx GDK_BACKEND wayland
-set -gx GBM_BACKEND nvidia-drm
-set -gx __GLX_VENDOR_LIVRARY_NAME nvidia
-set -gx WLR_NO_HARDWARE_CURSORS 1
-set -gx CLUTER_BACKEND wayland
-set -gx QT_QPA_PLATFORM wayland
-
-set -gx GDK_SCALE 1
-set -gx XCURSOR_SIZE 20
-set -gx RUST_BACKTRACE full
-set -gx DESKTOP_SESSION Hyprland
-set -gx JUPYTERLAB_DIR $HOME/.local/share/jupyter/lab
-set -gx SKIM_DEFAULT_OPTIONS "--ansi --regex"
-
+fish_add_path ~/.local/bin ~/.local/share
 set find_key_bindings fish_user_key_bindings
-set fish_cursor_default     block      blink
-set fish_cursor_insert      line       blink
-set fish_cursor_replace_one underscore blink
+set fish_cursor_default     block
+set fish_cursor_insert      line      
+set fish_cursor_replace_one underscore
 set fish_cursor_visual      block
+set -g fish_escape_delay_ms    100
 
 starship init fish | source
