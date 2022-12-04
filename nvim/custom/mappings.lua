2f5b556318a708bd649558a3e038ad09d32a3a30
liminchian <liminchian@gmail.com>
2022-12-04 11:51:35 +0800

    Last Sync: 2022-12-04 11:51:35

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -11,11 +11,11 @@ M.general = {
 
 M.dap = {
   n = {
-    ["<F9>"] = { ":lua require('dap').toggle_breakpoint()", "Toggle Breakpoint", opts = {} },
-    ["<F5>"] = { ":lua require('dap').continue()", "Continue execute", opts = {} },
-    ["<F11>"] = { ":lua require('dap').step_into()", "Step into", opts = {} },
-    ["<F10>"] = { ":lua require('dap').step_over()", "Step over", opts = {} },
-    ["<F12>"] = { ":lua require('dap').repl.open()", "Repl", opts = {} },
+    ["<F9>"] = { ":lua require('dap').toggle_breakpoint()<CR>", "Toggle Breakpoint", opts = {} },
+    ["<F5>"] = { ":lua require('dap').continue()<CR>", "Continue execute", opts = {} },
+    ["<F11>"] = { ":lua require('dap').step_into()<CR>", "Step into", opts = {} },
+    ["<F10>"] = { ":lua require('dap').step_over()<CR>", "Step over", opts = {} },
+    ["<F12>"] = { ":lua require('dap').repl.open()<CR>", "Repl", opts = {} },
   },
 }
 
