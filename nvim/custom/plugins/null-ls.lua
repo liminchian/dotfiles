6f2fd6c4183ce4249f99c0f37efff0a6af08cf8a
liminchian <liminchian@gmail.com>
2023-01-13 21:53:54 +0800

    Last Sync: 2023-01-13 21:53:54

--- nvim/custom/plugins/null-ls.lua
+++ nvim/custom/plugins/null-ls.lua
@@ -30,7 +30,23 @@ local sources = {
   b.diagnostics.mypy,
 
   -- Rust
-  b.formatting.rustfmt,
+  b.formatting.rustfmt.with {
+    extra_args = function(params)
+      local Path = require "plenary.path"
+      local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+      if cargo_toml:exists() and cargo_toml:is_file() then
+        for _, line in ipairs(cargo_toml:readlines()) do
+          local edition = line:match [[^edition%s*=%s*%"(%d+)%"]]
+          if edition then
+            return { "--edition=" .. edition }
+          end
+        end
+      end
+      -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+      return { "--edition=2021" }
+    end,
+  },
 }
 
 null_ls.setup { debug = true, sources = sources }
