4124eb170d0380ef862e77ca356a21027169061a
liminchian <liminchian@gmail.com>
2023-04-29 17:01:23 +0800

    Last Sync: 2023-04-29 17:01:23

--- fish/config.fish
+++ fish/config.fish
@@ -28,6 +28,7 @@ if status --is-login
     set -gx GLFW_IM_MODULE ibus
     set -gx OBSIDIAN $HOME/Desktop/obsidian-notes
     set -gx HYPRLAND_LOG_WLR 1
+    set -gx SAL_USE_VCLPLUGIN gtk3 
 
     exec Hyprland
   end
