4bf2cd3bfe09897609bfda85e79acc86cf85345e
liminchian <liminchian@gmail.com>
2023-08-10 20:46:54 +0800

    移除不必要檔案

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- example file i.e lua/custom/init.lua
--- require("my autocmds file") or just declare them here
-vim.cmd [[
-augroup change_cursor
-  au!
-  au ExitPre * :set guicursor=a:ver90
-augroup END
-]]
