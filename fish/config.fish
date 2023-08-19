6a05b0f4157c393a55aa0cc5050743f857e048d3
liminchian <liminchian@gmail.com>
2023-08-19 11:06:41 +0800

    Last Sync: 2023-08-19 11:06:41

--- fish/config.fish
+++ fish/config.fish
@@ -32,7 +32,7 @@ if test -e "$AUTOJUMP_PATH"
   source "$AUTOJUMP_PATH"
 end
 
-set find_key_bindings fish_user_key_bindings
+set find_key_bindings fish_user_key_bindings fish_mode_prompt
 set fish_cursor_default     block
 set fish_cursor_insert      line      
 set fish_cursor_replace_one underscore
